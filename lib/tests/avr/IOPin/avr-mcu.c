71487124e0f5ae215bf01bd64643f701fcbb276c
Kyrylo Yatsenko <kyrylo.yatsenko@hedrok.org>
2013-12-21 21:53:47 +0200

    Changed AVR file to cpp

---
+++ lib/tests/avr/IOPin/avr-mcu.c
@@ -0,0 +1,2 @@
+#include "avr/avr_mcu_section.h"
+AVR_MCU(16000000, "at90usb162");
--- lib/tests/avr/IOPin/avr-output-high.cpp
+++ lib/tests/avr/IOPin/avr-output-high.cpp
@@ -2,9 +2,6 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 
-#include "avr/avr_mcu_section.h"
-AVR_MCU(16000000, "at90usb162");
-
 int main()
 {	
     PORTC |= 1;
