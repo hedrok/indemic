abebbc85a9899eb9f798cee2150b5b9b307b1ae0
Kyrylo Yatsenko <kyrylo.yatsenko@hedrok.org>
2013-12-22 23:01:37 +0200

    Test refactoring. Run avr test in separate method

--- lib/tests/avr/IOPin/IOPinTest.h
+++ lib/tests/avr/IOPin/IOPinTest.h
@@ -11,17 +11,65 @@
 class IOPinTestSuite : public CxxTest::TestSuite
 {
     public:
+        void setUp()
+        {
+            this->portState = 0;
+            this->portChangesNum = 0;
+            this->portExpected.clear();
+        }
+
+        void runTest(const char* fname, int cycles)
+        {
+            elf_firmware_t f;
+            elf_read_firmware(fname, &f);
+
+            avr_t * avr = NULL;
+
+            avr = avr_make_mcu_by_name(f.mmcu);
+            if (!avr) {
+                TS_FAIL("avr_make_mcu failed");
+                return;
+            }
+
+            avr_init(avr);
+            avr_load_firmware(avr, &f);
+
+            for (int i = 0; i < 8; i++) {
+                avr_irq_register_notify(
+                    avr_io_getirq(avr, AVR_IOCTL_IOPORT_GETIRQ('C'), i),
+                    [](struct avr_irq_t * irq, uint32_t value, void *param) {
+                        IOPinTestSuite *p = static_cast<IOPinTestSuite*>(param);
+                        if (value) {
+                            p->portState |= (1 << irq->irq);
+                        } else {
+                            p->portState &= ~(1 << irq->irq);
+                        }
+                        if (p->portChangesNum < p->portExpected.size()) {
+                            TS_ASSERT_EQUALS(
+                                p->portState,
+                                p->portExpected[p->portChangesNum]
+                            );
+                        }
+                        p->portChangesNum++;
+                    },
+                    static_cast<void*>(this)
+                );
+            }
+
+            for (int i = 0; i < cycles; i++) {
+		        avr_run(avr);
+            }
+
+            TS_ASSERT_EQUALS(portChangesNum, this->portExpected.size());
+        }
+
         /**
-         * testing IOPin::setHigh, 
+         * testing IOPin::setHigh,
          *         IOPin::setLow,
          *         IOPin::setValue
          */
         void testOutput()
         {
-            this->portState = 0;
-            this->changesNum = 0;
-            this->portExpected.clear();
-
             // First set 0 to high
             this->portExpected.push_back(0x1);
             // 7 messages come after change
@@ -49,52 +97,11 @@ class IOPinTestSuite : public CxxTest::TestSuite
             // setting 1 to low using setValue
             this->portExpected.push_back(0x0);
 
-            elf_firmware_t f;
-            const char * fname = "avr-output.elf";
-            elf_read_firmware(fname, &f);
-
-            avr_t * avr = NULL;
-
-            avr = avr_make_mcu_by_name(f.mmcu);
-            if (!avr) {
-                TS_FAIL("avr_make_mcu failed");
-                return;
-            }
-
-            avr_init(avr);
-            avr_load_firmware(avr, &f);
-
-            for (int i = 0; i < 8; i++) {
-                avr_irq_register_notify(
-                    avr_io_getirq(avr, AVR_IOCTL_IOPORT_GETIRQ('C'), i),
-                    [](struct avr_irq_t * irq, uint32_t value, void *param) {
-                        IOPinTestSuite *p = static_cast<IOPinTestSuite*>(param);
-                        if (value) {
-                            p->portState |= (1 << irq->irq);
-                        } else {
-                            p->portState &= ~(1 << irq->irq);
-                        }
-                        if (p->changesNum < p->portExpected.size()) {
-                            TS_ASSERT_EQUALS(
-                                p->portState,
-                                p->portExpected[p->changesNum]
-                            );
-                        }
-                        p->changesNum++;
-                    },
-                    static_cast<void*>(this)
-                );
-            }
-
-            for (int i = 0; i < 100; i++) {
-		        avr_run(avr);
-            }
-
-            TS_ASSERT_EQUALS(changesNum, this->portExpected.size());
+            runTest("avr-output.elf", 100);
         }
 
     private:
         uint8_t portState;
-        int changesNum;
+        size_t portChangesNum;
         std::vector<uint8_t> portExpected;
 };
