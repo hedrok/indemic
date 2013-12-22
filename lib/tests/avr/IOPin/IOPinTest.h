4cde9b478dbfb5414d028b50c7056db314a1fee2
Kyrylo Yatsenko <kyrylo.yatsenko@hedrok.org>
2013-12-22 22:02:26 +0200

    Moved global declarations into test class

--- lib/tests/avr/IOPin/IOPinTest.h
+++ lib/tests/avr/IOPin/IOPinTest.h
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include <cxxtest/TestSuite.h>
 
 #include "sim_avr.h"
@@ -6,51 +8,47 @@
 #include "sim_gdb.h"
 #include "sim_vcd_file.h"
 
-uint8_t	pin_state = 0;	// current port C value
-
-static int callNum = 0;
-static uint8_t expected[] = 
-{
-    // First set 0 to high
-    0x1,
-    // 7 messages come after change
-    // of first pin
-    0x1,
-    0x1,
-    0x1,
-    0x1,
-    0x1,
-    0x1,
-    0x1,
-    // Set 1 to high
-    0x3,
-    // Set 0 to low
-    0x2,
-    // Set 1 to low
-    0x0,
-    // Testing setValue function:
-    // setting 1 to high using setValue
-    0x2,
-    // setting 0 to high using setValue
-    0x3,
-    // setting 0 to low using setValue
-    0x2,
-    // setting 1 to low using setValue
-    0x0
-};
-void pin_changed_hook(struct avr_irq_t * irq, uint32_t value, void * param)
-{
-	pin_state = (pin_state & ~(1 << irq->irq)) | (value << irq->irq);
-    TS_ASSERT_EQUALS(pin_state, expected[callNum]);
-    callNum++;
-}
-
 class IOPinTestSuite : public CxxTest::TestSuite
 {
     public:
-        // testing setHigh, setLow, setValue
-        void testOutput(void)
+        /**
+         * testing IOPin::setHigh, 
+         *         IOPin::setLow,
+         *         IOPin::setValue
+         */
+        void testOutput()
         {
+            this->portState = 0;
+            this->changesNum = 0;
+            this->portExpected.clear();
+
+            // First set 0 to high
+            this->portExpected.push_back(0x1);
+            // 7 messages come after change
+            // of first pin
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            this->portExpected.push_back(0x1);
+            // Set 1 to high
+            this->portExpected.push_back(0x3);
+            // Set 0 to low
+            this->portExpected.push_back(0x2);
+            // Set 1 to low
+            this->portExpected.push_back(0x0);
+            // Testing setValue function:
+            // setting 1 to high using setValue
+            this->portExpected.push_back(0x2);
+            // setting 0 to high using setValue
+            this->portExpected.push_back(0x3);
+            // setting 0 to low using setValue
+            this->portExpected.push_back(0x2);
+            // setting 1 to low using setValue
+            this->portExpected.push_back(0x0);
+
             elf_firmware_t f;
             const char * fname = "avr-output.elf";
             elf_read_firmware(fname, &f);
@@ -66,16 +64,37 @@ class IOPinTestSuite : public CxxTest::TestSuite
             avr_init(avr);
             avr_load_firmware(avr, &f);
 
-            for (int i = 0; i < 8; i++)
+            for (int i = 0; i < 8; i++) {
                 avr_irq_register_notify(
                     avr_io_getirq(avr, AVR_IOCTL_IOPORT_GETIRQ('C'), i),
-                    pin_changed_hook, 
-                    NULL);
+                    [](struct avr_irq_t * irq, uint32_t value, void *param) {
+                        IOPinTestSuite *p = static_cast<IOPinTestSuite*>(param);
+                        if (value) {
+                            p->portState |= (1 << irq->irq);
+                        } else {
+                            p->portState &= ~(1 << irq->irq);
+                        }
+                        if (p->changesNum < p->portExpected.size()) {
+                            TS_ASSERT_EQUALS(
+                                p->portState,
+                                p->portExpected[p->changesNum]
+                            );
+                        }
+                        p->changesNum++;
+                    },
+                    static_cast<void*>(this)
+                );
+            }
 
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < 100; i++) {
 		        avr_run(avr);
             }
 
-            TS_ASSERT_EQUALS(callNum, sizeof(expected) / sizeof(expected[0]));
+            TS_ASSERT_EQUALS(changesNum, this->portExpected.size());
         }
+
+    private:
+        uint8_t portState;
+        int changesNum;
+        std::vector<uint8_t> portExpected;
 };
