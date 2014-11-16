9ace08ca40c301350a8763ae788fa719ff6061a1
Kyrylo Yatsenko <kyrylo.yatsenko@hedrok.org>
2014-11-16 17:00:46 +0200

    Uart test improvement
    Make testing function more generic: move sent and received strings
    to arguments, so that other tests can be easily added

--- lib/tests/avr/Uart/UartTest.t.h
+++ lib/tests/avr/Uart/UartTest.t.h
@@ -12,11 +12,15 @@ class UartTestSuite : public CxxTest::TestSuite
          * Run test that checks Uart - each sent symbol should
          * be increased by one
          * @param fname Filename of avr firmware
+         * @param send Characters to send via UART
+         * @param expect Expected string to receive via UART
          */
-        void runTest(const char* fname)
+        void runTest(const char* fname, const char * send, const char * expect)
         {
             elf_firmware_t f;
             elf_read_firmware(fname, &f);
+            this->_curSend = send;
+            this->_curExpect = expect;
 
             avr = avr_make_mcu_by_name(f.mmcu);
             if (!avr) {
@@ -27,30 +31,29 @@ class UartTestSuite : public CxxTest::TestSuite
             avr_init(avr);
             avr_load_firmware(avr, &f);
 
-            _callnum = 0;
-            _testByte = 'a';
 	        avr_irq_t * uart_output = avr_io_getirq(avr, AVR_IOCTL_UART_GETIRQ('1'), UART_IRQ_OUTPUT);
 	        avr_irq_t * uart_xon = avr_io_getirq(avr, AVR_IOCTL_UART_GETIRQ('1'), UART_IRQ_OUT_XON);
             avr_irq_register_notify(
                 uart_output,
                 [](struct avr_irq_t * irq, uint32_t value, void *param) {
                     UartTestSuite * _this = static_cast<UartTestSuite*>(param);
-                    _this->_callnum++;
-                    TS_ASSERT_EQUALS(value, _this->_testByte + 1);
+                    if (*_this->_curExpect == '\0') {
+                        TS_FAIL("Too many bytes received\n");
+                        return;
+                    }
+                    TS_ASSERT_EQUALS(value, *_this->_curExpect++);
                 },
                 static_cast<void*>(this)
             );
             avr_irq_register_notify(
                 uart_xon,
                 [](struct avr_irq_t * irq, uint32_t value, void *param) {
-                    static bool called = false;
-                    if (called) {
+                    UartTestSuite * _this = static_cast<UartTestSuite*>(param);
+                    if (*_this->_curSend == '\0') {
                         return;
                     }
-                    called = true;
-                    UartTestSuite * _this = static_cast<UartTestSuite*>(param);
 	                avr_irq_t * uart_input = avr_io_getirq(_this->avr, AVR_IOCTL_UART_GETIRQ('1'), UART_IRQ_INPUT);
-		            avr_raise_irq(uart_input, _this->_testByte);
+		            avr_raise_irq(uart_input, *_this->_curSend++);
                 },
                 static_cast<void*>(this)
             );
@@ -59,8 +62,9 @@ class UartTestSuite : public CxxTest::TestSuite
             for (avr_cycle_count_t i = 0; i < 2000; i++) {
                 avr_run(avr);
             }
-            // Check that Uart character was sent
-            TS_ASSERT_EQUALS(this->_callnum, 1);
+            // Check that all Uart character were sent/received
+            TS_ASSERT_EQUALS(*this->_curExpect, '\0');
+            TS_ASSERT_EQUALS(*this->_curSend, '\0');
         }
 
         /**
@@ -68,11 +72,11 @@ class UartTestSuite : public CxxTest::TestSuite
          */
         void testUart()
         {
-            runTest("Uart/uart.at90usb162.avr.elf");
+            runTest("Uart/uart.at90usb162.avr.elf", "abc", "bcd");
         }
 
     private:
         avr_t * avr;
-        int _callnum;
-        uint8_t _testByte;
+        const char * _curSend;
+        const char * _curExpect;
 };
